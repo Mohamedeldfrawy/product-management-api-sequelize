3d9c0264dcc3473f68a87eb19e77becbeffdb02a
Mohamed Eldfrawy <mohmmedeldfrawy@gmail.com>
2026-04-13 11:33:37 +0200

    change database to sequelize

--- db/db.connect.js
+++ db/db.connect.js
@@ -10,21 +10,36 @@
 // }
 
 
-import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  'mysql://ufihg2wigko9pdsd:yhNDaEEIMVVRIwKtQab7@beeyxkun52fbuf4mmjtx-mysql.services.clever-cloud.com:3306/beeyxkun52fbuf4mmjtx',
-  {
-    dialect: 'mysql',
-    logging: false,
 
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
 
-export { sequelize };
+import { Sequelize}  from 'sequelize'
+const sequelize = new Sequelize ('sequelize','root','',{
+
+host:'localhost',
+dialect:'mysql'
+})
+
+export{
+sequelize
+}
+
+
+// import { Sequelize } from 'sequelize';
+
+// const sequelize = new Sequelize(
+//   'mysql://ufihg2wigko9pdsd:yhNDaEEIMVVRIwKtQab7@beeyxkun52fbuf4mmjtx-mysql.services.clever-cloud.com:3306/beeyxkun52fbuf4mmjtx',
+//   {
+//     dialect: 'mysql',
+//     logging: false,
+
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+// );
+
+// export { sequelize };
